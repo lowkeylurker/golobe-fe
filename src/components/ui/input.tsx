9f14ac13c5c3d38cdcc0edc4b11cfb84e7c8ef55
lowkeylurker <liem.levicoding834124@gmail.com>
2026-01-10 11:43:07 +0700

    Add Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,19 +2,39 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
-function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
+function Input({
+  className,
+  type,
+  label,
+  ...props
+}: React.ComponentProps<'input'> & {
+  label: string
+}) {
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-        'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-        className
-      )}
-      {...props}
-    />
+    <div className="relative">
+      <input
+        type={type}
+        data-slot="input"
+        placeholder=" "
+        className={cn(
+          'font-montserrat border-input-border file:text-foreground selection:bg-primary selection:text-primary-foreground peer h-14 w-full min-w-52.5 rounded-md border bg-transparent px-4 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-transparent',
+          'disabled:border-secondary disabled:text-secondary-foreground disabled:pointer-events-none disabled:cursor-not-allowed',
+          'focus:border-primary-focus focus:placeholder:text-foreground focus:border-2',
+          'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+          className
+        )}
+        {...props}
+      />
+      <label
+        htmlFor={props.id}
+        className={cn(
+          'font-montserrat bg-background text-foreground peer-focus:text-primary-focus peer-disabled:text-secondary-foreground absolute top-2 left-3 z-10 origin-left -translate-y-4 scale-[87.5%] transform px-2 text-base duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1 peer-focus:-translate-y-4 peer-focus:scale-[87.5%] peer-focus:px-2',
+          className?.includes('border-destructive') ? 'text-destructive' : ''
+        )}
+      >
+        {label}
+      </label>
+    </div>
   )
 }
 
